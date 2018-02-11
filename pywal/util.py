b97ac7f7e69ce939e711c49f410eb2a3482887c5
Dylan Araps <dylan.araps@gmail.com>
2018-02-12 09:23:16 +1100

    colors: Added dynamic iterm template

--- pywal/util.py
+++ pywal/util.py
@@ -103,6 +103,24 @@ class Color:
         percent = float(re.sub(r'[\D\.]', '', str(percent)))
         return Color(saturate_color(self.hex_color, percent / 100))
 
+    @property
+    def r(self):
+        """Red channel."""
+        r = (*hex_to_rgb(self.hex_color),)[0]
+        return r/255
+
+    @property
+    def g(self):
+        """Green channel."""
+        g = (*hex_to_rgb(self.hex_color),)[1]
+        return g/255
+
+    @property
+    def b(self):
+        """Blue channel."""
+        b = (*hex_to_rgb(self.hex_color),)[2]
+        return b/255
+
 
 def read_file(input_file):
     """Read data from a file and trim newlines."""
